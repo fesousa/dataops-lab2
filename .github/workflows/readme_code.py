425dc228e0b37033e7880f7e0d5bc113a365a774
Fernando Sousa <fernandosousa.ti@gmail.com>
2021-10-31 11:05:18 -0300

    melhorias README.md e license

--- .github/workflows/readme_code.py
+++ .github/workflows/readme_code.py
@@ -4,6 +4,7 @@ with open('README.md', 'r') as readme:
     subs = re.findall(r'```.*?\n(\$\{.*?\})\n```', content)
     for s in subs:
         file = s[2:-1]
+        print(file)
         with open(file) as f:
             content.replace(s, f.read())
     
